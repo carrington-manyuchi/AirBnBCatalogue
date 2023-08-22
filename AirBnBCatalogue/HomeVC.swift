bc2024975fa28bc41c2df413a8da1c2ec3ffcb3c
Carrington <Manyuchi>
2023-08-22 13:24:23 +0200

    UITableView

---
+++ AirBnBCatalogue/HomeVC.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  AirBnBCatalogue
+//
+//  Created by DA MAC M1 157 on 2023/08/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.allowsSelection = true
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupUI()
+        
+    }
+    
+    func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        setupConstraints()
+    }
+    
+    
+    func setupConstraints() {
+        let composeTableView = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(composeTableView)
+    }
+
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Carrington"
+        cell.detailTextLabel?.text = "Manyuchi"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
