a8df197937b10b6938132176d7fc956664eca49f
Carrington <Manyuchi>
2023-08-22 18:42:34 +0200

    CALayer

--- AirBnBCatalogue/HomeVC.swift
+++ AirBnBCatalogue/HomeVC.swift
@@ -9,23 +9,34 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    private let images: [UIImage] = [
+        UIImage(named: "1")!,
+        UIImage(named: "2")!,
+        UIImage(named: "3")!,
+        UIImage(named: "4")!,
+        UIImage(named: "5")!,
+        UIImage(named: "6")!,
+        UIImage(named: "7")!,
+        UIImage(named: "8")!
+        
+    ]
+    
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(RecommendedTableViewCell.self, forCellReuseIdentifier: RecommendedTableViewCell.identifier)
         tableView.allowsSelection = true
         return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         setupUI()
-        
     }
     
     func setupUI() {
@@ -53,17 +64,24 @@ class HomeVC: UIViewController {
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return images.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Carrington"
-        cell.detailTextLabel?.text = "Manyuchi"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecommendedTableViewCell.identifier, for: indexPath) as? RecommendedTableViewCell else {
+            fatalError("The tableView could not dequeue RecommendedTableViewCell in the view controller")
+        }
+        
+        let image = self.images[indexPath.row]
+        cell.configure(with: image)
         return cell
+        
+//        cell.textLabel?.text = "Carrington"
+//        cell.detailTextLabel?.text = "Manyuchi"
+//        return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 125
+//    }
 }
--- AirBnBCatalogue/RecommendedTableViewCell.swift
+++ AirBnBCatalogue/RecommendedTableViewCell.swift
@@ -1,7 +1,6 @@
 //
 //  RecommendedTableViewCell.swift
 //  AirBnBCatalogue
-//
 //  Created by DA MAC M1 157 on 2023/08/22.
 //
 
@@ -10,11 +9,28 @@ import UIKit
 class RecommendedTableViewCell: UITableViewCell {
     
     public static let identifier = "RecommendedTableViewCell"
+    
+    private let backView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 7
+        view.layer.shadowOpacity = 0.7
+        view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        view.layer.shadowOffset = CGSize(width: 3, height: 2)
+        view.layer.shadowRadius = 5
+        
+        view.layer.borderColor = UIColor.red.cgColor
+        view.layer.backgroundColor = UIColor.systemCyan.cgColor
+        view.layer.borderWidth = 2
+        return view
+    }()
 
-    private let image: UIImageView = {
+    private let myImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleToFill
+        imageView.image = UIImage(systemName: "person")
+        imageView.layer.masksToBounds = true
         return  imageView
     }()
     
@@ -23,25 +39,38 @@ class RecommendedTableViewCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Title"
         label.textColor = UIColor.label
-        label.font = UIFont(name: "avenir-bold", size: 25)
+        label.font = UIFont(name: "avenir-bold", size: 18)
         label.numberOfLines = 1
         return label
     }()
     
-    
     private let subTitle: UILabel = {
-        let
-    }
-    
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Subtitle"
+        label.font = UIFont(name: "avenir-medium", size: 25)
+        label.textColor = UIColor.systemGray5
+        label.numberOfLines = 1
+        return label
+    }()
+        
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Price"
+        label.font = UIFont(name: "avenir-medium", size: 15)
+        label.textColor = UIColor.label
+        label.numberOfLines = 1
+        return label
+    }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        setupUI()
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -50,6 +79,64 @@ class RecommendedTableViewCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with image: UIImage) {
+        myImageView.image = image
+    }
+    
+    func setupUI() {
+        self.contentView.addSubview(backView)
+        self.contentView.addSubview(myImageView)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(subTitle)
+        self.contentView.addSubview(priceLabel)
+        setupConstraints()
+        
+        
+        
+    }
+    
+    func setupConstraints() {
+        
+        let composeBackView = [
+            backView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            backView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            backView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            backView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ]
+        
+        let composeImage = [
+            myImageView.topAnchor.constraint(equalTo: backView.topAnchor, constant: 10),
+            myImageView.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: 10),
+            myImageView.bottomAnchor.constraint(equalTo: backView.bottomAnchor, constant: -10),
+            myImageView.widthAnchor.constraint(equalToConstant: 90),
+            myImageView.heightAnchor.constraint(equalToConstant: 90)
+        ]
+        let composeTitleLabel = [
+            titleLabel.topAnchor.constraint(equalTo: myImageView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: myImageView.trailingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -10)
+        ]
+        
+        let composeSubTitle = [
+            subTitle.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2),
+            subTitle.leadingAnchor.constraint(equalTo: myImageView.trailingAnchor, constant: 10),
+            subTitle.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -10)
+        ]
+        
+        let composePriceLabel = [
+            priceLabel.topAnchor.constraint(equalTo: subTitle.bottomAnchor, constant: 10),
+            priceLabel.bottomAnchor.constraint(equalTo: myImageView.bottomAnchor),
+            priceLabel.leadingAnchor.constraint(equalTo: myImageView.trailingAnchor, constant: 10),
+            priceLabel.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -10)
+        ]
+    
+        NSLayoutConstraint.activate(composeImage)
+        NSLayoutConstraint.activate(composeTitleLabel)
+        NSLayoutConstraint.activate(composeSubTitle)
+        NSLayoutConstraint.activate(composePriceLabel)
+        NSLayoutConstraint.activate(composeBackView)
     }
 
 }
